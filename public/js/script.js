8f306b9cbb77fe8984776eca83ace9ce93e85d56
Brayan <brayanjulioquispe@gmail.com>
2024-03-19 22:52:00 -0400

    sliderbar

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    const content = document.querySelector('.content');
+    sidebar.classList.toggle('active');
+    content.classList.toggle('active');
+  }
