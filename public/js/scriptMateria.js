b7314628a93b8967d19f13b90619cd7eda2c8834
Jhosemar <jhoten703@gmail.com>
2024-03-31 00:53:07 -0400

    Mejora de frontend Materia

--- public/js/scriptMateria.js
+++ public/js/scriptMateria.js
@@ -1,5 +1,8 @@
 const sub1 = document.getElementById("sub1");
 const sub2 = document.getElementById("sub2");
+const btnMenu = document.getElementById("btnMenu");
+const menu = document.getElementById("menu");
+
 
 function gesAmbiente(){
     if (sub1.style.display === 'none' || sub1.style.display === '') {
@@ -16,6 +19,16 @@ function gesUnidad(){
         //btnPanelUsr.classList.add('seleccionado');
     } else {
         sub2.style.display = 'none';
+        //btnPanelUsr.classList.remove('seleccionado');
+    }
+}
+
+function desMenu(){
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'flex';
+        //btnPanelUsr.classList.add('seleccionado');
+    } else {
+        menu.style.display = 'none';
         //btnPanelUsr.classList.remove('seleccionado');
     }
 }
--- resources/views/materia.blade.php
+++ resources/views/materia.blade.php
@@ -9,9 +9,8 @@
 </head>
 <body>
     <div class="contenedor">
-        <div class="izquierda">
-            <h1><i class='fas fa-bars'></i> Menu</h1>
-            <nav class="menu">
+        <div class="izquierda" id="menu">
+            <nav class="menu" >
                 <ul>
                     <li>
                         <a href="#"><i class='fas fa-home'></i> Inicio</a>
@@ -61,15 +60,75 @@
         </div>
         <div class="derecha">
             <header>
-                <p>header</p>
+                <h1 id="btnMenu" onclick="desMenu()"><i class='fas fa-bars'></i> Menu</h1>
+                <i class='fas fa-user'></i>
             </header>
+            <br>
             <main>
                 <div>
-                    <h1><i class='fas fa-graduation-cap'></i> Registrar Carrera</h1>
+                    <h1><i class='fas fa-book'></i> Registrar Materia</h1>
+                    <form class="materia" method="POST" enctype="multipart/form-data" id="materia">
+                        <div class="input-group">
+                            <label class="labMateria">Departamento</label>
+                            <select class="inputMateria" id="departamento" name="departamento">
+                                <option value="">Seleccione una opcion</option> 
+                                <option value="2022">2022</option> 
+                                <option value="2021">2021</option> 
+                                <option value="2020">2020</option> 
+                            </select>
+                        </div>
+
+                        <div class="input-group">
+                            <label class="labMateria">Carrera</label>
+                            <select class="inputMateria" id="carrera" name="carrera">
+                                <option value="">Seleccione una opcion</option> 
+                                <option value="2023">Ing. de Sistemas</option> 
+                                <option value="2022">Ing. Informatica</option> 
+                                <option value="2021">Ing. Industrial</option> 
+                                <option value="2020">Ing. Quimica</option> 
+                            </select>
+                        </div>
+
+                        <div class="input-group">
+                            <label class="labMateria" id="labMateria">Nombre</label>
+                            <input required name="nombre" class="inputMateria" id="nomPl" placeholder="Max. 100 caracteres" oninput="verificarContorno(this)">
+                        </div>
+                        <div class="input-group">
+                            <label class="labMateria" id="labMateria">Codigo</label>
+                            <input required name="codigo" autocomplete="off" class="inputMateria" id="nomPl" placeholder="Max. 100 caracteres" oninput="verificarContorno(this)">
+                        </div>
+
+                        <div class="input-group">
+                            <label class="labMateria">Nivel</label>
+                            <select class="inputMateria" id="nivel" name="nivel">
+                                <option value="">Seleccione una opcion</option> 
+                                <option value="2022">2022</option> 
+                                <option value="2021">2021</option> 
+                                <option value="2020">2020</option> 
+                            </select>
+
+                            <label class="labMateria">Grupo</label>
+                            <select class="inputMateria" id="grupo" name="grupo">
+                                <option value="">Seleccione una opcion</option> 
+                                <option value="2022">2022</option> 
+                                <option value="2021">2021</option> 
+                                <option value="2020">2020</option> 
+                            </select>
+                        </div>
+                        
+                        <div class="botones">
+                            <button onclick="enviarFormulario()" type="button" class="btnCancelar">Cancelar</button>
+                            <button onclick="enviarFormulario()" type="button" class="btnRegistrar">Registrar</button>
+                        </div>
+                    </form>
                 </div>
             </main>
         </div>
     </div>
+    <footer>
+        <br>
+        <p>Este es un footer</p>
+    </footer>
 </body>
 <script src="{{asset('js/scriptMateria.js')}}"></script>
 </html>
