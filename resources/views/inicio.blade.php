e80d60c8b3bdac2cb5d2b39140b74ffc500f4f18
Jhosemar <jhoten703@gmail.com>
2024-04-05 03:31:11 -0400

    Diseño de Inicio rapido

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,4 +1,31 @@
 @extends('layoutes.plantilla')
+
+@section('estilos')
+    <style>
+        .ini{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center; 
+            flex-direction: column;	
+            gap: 20px;
+        }
+        .mensajeIni{
+            font-weight: bold;
+            font-size: 42px;
+        }
+        .logo2{
+            width: 200px;
+            height: auto;
+            
+        }
+    </style>
+@endsection
+
 @section('contenido')
-    pagina principal
+    <div class="ini">
+        <img src="{{asset('images\logo.png')}}" alt="Logo" class="logo2">
+        <h1 class="mensajeIni">Inicio Smart Byte</h1>
+    </div>
 @endsection
