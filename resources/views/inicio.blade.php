08a5efea8ea95c248761fa64656c8d59894f18fc
Jhosemar <jhoten703@gmail.com>
2024-04-05 03:34:12 -0400

    Diseño de Inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -24,8 +24,5 @@
 @endsection
 
 @section('contenido')
-    <div class="ini">
-        <img src="{{asset('images\logo.png')}}" alt="Logo" class="logo2">
-        <h1 class="mensajeIni">Inicio Smart Byte</h1>
-    </div>
+    pagina principal
 @endsection
