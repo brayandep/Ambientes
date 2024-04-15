38b93782e696b8525c8e80a4c0aed5979b31aeaa
Katherine Ortiz <katherineortiz440@gmail.com>
2024-04-15 03:07:36 -0400

    mostrar-ambientes-orden-corregido

--- app/Http/Controllers/BuscadorController.php
+++ app/Http/Controllers/BuscadorController.php
@@ -11,12 +11,20 @@ class BuscadorController extends Controller
 {
     public function show(){
         $ambientes = Ambiente::orderBy('nombre', 'asc')->get();
-        $horarios = HorarioDisponible::where('estadoHorario', 0)->get();
+
+        // Definir el orden personalizado de los días de la semana de lunes a sábado
+        $ordenDias = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+
+        $horarios = HorarioDisponible::where('estadoHorario', 0)
+        ->orderByRaw("FIELD(dia, '" . implode("','", $ordenDias) . "')") // Ordenar por días personalizados
+        ->orderBy('horaInicio', 'asc') // Luego ordenar por hora de inicio
+        ->get();
+        
         return view('Buscador.Buscador', compact('ambientes', 'horarios'));
     }
 
     public function search(BuscadorR $request)
-{
+    {
     $validatedData = $request->validated();
 }
 }
