1fae5f4df38bba0880485b60611b3fe71045a29f
eliasra07 <202001834@est.umss.edu>
2024-04-17 20:56:33 -0400

    prueba_estilos

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,4 +1,11 @@
 @extends('layoutes.plantilla')
 @section('contenido')
-    pagina principal
+<style>
+    .prueba {
+    font-size: 18px;
+    color: #333;
+    margin-top: 20px;
+}
+</style>
+    <p class="prueba">pagina principal</p>
 @endsection
