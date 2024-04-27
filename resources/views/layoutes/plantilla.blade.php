ede6522646fec41541ff4e113bd1ed7c22a4401f
Jhosemar <jhoten703@gmail.com>
2024-04-27 17:58:08 -0400

    'url' en vez de 'asset'

--- resources/views/layoutes/plantilla.blade.php
+++ resources/views/layoutes/plantilla.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/stylePlantilla.css')}}">
+    <link rel="stylesheet" href="{{url('css/stylePlantilla.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
     @yield('links')
@@ -119,7 +119,7 @@
         </div>          
     </footer>
 </body>
-<script src="{{asset('js/scriptPlantilla.js')}}"></script>
+<script src="{{url('js/scriptPlantilla.js')}}"></script>
 @yield('scripts')
 </html>
 
