294d648d974fa08549083f34d34db2324deea58e
Katherine Ortiz <katherineortiz440@gmail.com>
2024-05-15 00:42:55 -0400

    modal-restaurar-eliminar-implementado

--- resources/views/backup/backup.blade.php
+++ resources/views/backup/backup.blade.php
@@ -42,19 +42,19 @@
                         <div class="fila-backup">
                             <p>{{ $backup }}</p>
                             <div id="columnaPeque" class="accion-backup">
-                                <form action="{{ route('backup.restore') }}" method="POST">
+                                <form id="restaurarForm_{{ $backup }}" action="{{ route('backup.restore') }}" method="POST">
                                     @csrf
                                     <input type="hidden" name="restorePoint" value="{{ $backup }}">
-                                    <button type="submit" class="accion">
+                                    <button type="button" class="accion" onclick="RestaurarBackup('{{ $backup }}')">
                                         <i class="fa-solid fa-clock-rotate-left"></i>
                                     </button>
                                 </form>
                             </div>
                             <div id="columnaPeque" class="accion-backup">
-                                <form action="{{ route('backup.destroy', $backup) }}" method="POST">
+                                <form id="eliminarForm_{{ $backup }}" action="{{ route('backup.destroy', $backup) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="accion">
+                                    <button type="button" class="accion" onclick="EliminarBackup('{{ $backup }}')">
                                         <i class="fa-solid fa-trash"></i>
                                     </button>
                                 </form>
@@ -64,5 +64,61 @@
                 </div>
             </div>
         </div>
+
+        <div id="fondoGris"></div>
+        <div class="panel" id="panelEliminar">
+              <p>¿Esta seguro que desea eliminar el backup de forma permanente?</p>
+              <div class="btnPanel">
+                <button class= "no" onclick="cancelarEliminar()" >No</button>
+                <button class="si" onclick="confirmarEliminar()">Si</button>
+            </div>
+        </div>
+        <div class="panel" id="panelRestaurar">
+            <p>¿Está seguro que desea restaurar el backup a la fecha y hora indicada?</p>
+            <div class="btnPanel">
+                <button class= "no" onclick="cancelarRestaurar()" >No</button>
+                <button class="si" onclick="confirmarRestaurar()">Si</button>
+            </div>
+        </div>
     </div>
+@endsection
+
+@section('scripts')
+{{-- modal restaurar y eleminar--}}
+<script>
+    let backupToDelete;
+    let backupToRestore;
+
+    function EliminarBackup(backup){
+        backupToDelete = backup;
+        panelEliminar.style.display = 'block';
+        fondoGris.style.display = 'flex';
+    }
+    
+    function cancelarEliminar(){
+        panelEliminar.style.display  = 'none';
+        fondoGris.style.display = 'none';
+    }
+
+    function confirmarEliminar() {
+        const formId = 'eliminarForm_' + backupToDelete;
+        document.getElementById(formId).submit();
+    }
+
+    function RestaurarBackup(backup){
+        backupToRestore = backup;
+        panelRestaurar.style.display = 'block';
+        fondoGris.style.display = 'flex';
+    }
+
+    function cancelarRestaurar(){
+        panelRestaurar.style.display  = 'none';
+        fondoGris.style.display = 'none';
+    }
+
+    function confirmarRestaurar() {
+        const formId = 'restaurarForm_' + backupToRestore;
+        document.getElementById(formId).submit();
+    }
+</script>
 @endsection
