d2a35b93b085e0d9f35821109dc856aeeeb10616
Jhosemar <jhoten703@gmail.com>
2024-05-17 19:45:25 -0400

    Botones del menu restringidos por rol

--- resources/views/layoutes/plantilla.blade.php
+++ resources/views/layoutes/plantilla.blade.php
@@ -26,51 +26,74 @@
                     <li>
                         <a href="{{ route('calendario.index') }}"><i class='fas fa-calendar-days'></i> Calendario</a>
                     </li>
-                    <li onclick="gesAmbiente()">
-                        <p><i class='fas fa-clipboard'></i> Gestionar Ambiente</p>
-                        
-                    </li>
+                    @if (Auth::user()->can('Ver ambiente') || Auth::user()->can('Regsitrar ambiente'))
+                        <li onclick="gesAmbiente()">
+                            <p><i class='fas fa-clipboard'></i> Gestionar Ambiente</p>
+                            
+                        </li>
+                    @endif
                     <nav class="subMenu" id="sub1">
                         <ul>
+                            @can('Regsitrar ambiente')   
                             <li>
                                 <a href="{{ route('ambiente.create') }}"><i class='fas fa-clipboard'></i> Registrar Ambiente</a>
                             </li>
+                            @endcan
+                            @can('Ver ambiente')   
                             <li>
                                 <a href="{{ route('AmbientesRegistrados') }}"><i class="fa-solid fa-rectangle-list"></i> Ver Informacion de ambiente</a>
                             </li>
+                            @endcan
                         </ul>
                     </nav>
-                    <li onclick="gesUnidad()">
-                        <p><i class='fas fa-clipboard'></i> Gestionar Unidad</p>
-                    </li>
+
+                    @if (Auth::user()->can('Ver unidad') || Auth::user()->can('Registrar unidad'))
+                        <li onclick="gesUnidad()">
+                            <p><i class='fas fa-clipboard'></i> Gestionar Unidad</p>
+                        </li>
+                    @endif
                     <nav class="subMenu" id="sub2">
                         <ul>
+                            @can('Registrar unidad') 
                             <li>
                                 <a href='{{ route('unidad.registrar') }}'><i class="fas fa-building"></i> Registrar unidad nueva</a>
                             </li>
+                            @endcan
+                            @can('Ver unidad') 
                             <li>
                                 <a href='{{ route('visualizar_unidad') }}'><i class='fas fa-clipboard'></i> Visualizar unidad</a>
                             </li>
+                            @endcan
                         </ul>
                     </nav>
-                    <li onclick="gesMateria()">
-                        <p><i class='fas fa-clipboard'></i> Gestionar Materia</p>
-                    </li>
+
+                    @if (Auth::user()->can('Ver materia') || Auth::user()->can('Registrar materia'))
+                        <li onclick="gesMateria()">
+                            <p><i class='fas fa-clipboard'></i> Gestionar Materia</p>
+                        </li>
+                    @endif
+                    
                     <nav class="subMenu" id="subMateria">
                         <ul>
+                            @can('Registrar materia') 
                             <li>
                                 <a href='{{ route('materia.reg') }}'><i class="fas fa-book"></i> Registrar Materia</a>
                             </li>
+                            @endcan
+                            @can('Ver materia')
                             <li>
                                 <a href='{{ route('materia.show') }}'><i class='fas fa-rectangle-list'></i> Lista de Materias</a>
                             </li>
+                            @endcan
                         </ul>
                     </nav>
-                    <li onclick="gesReserva()">
-                        <p><i class='fas fa-clipboard'></i> Gestionar mis solicitudes</p>
-
                     
-                    </li>
+                    @can('Solicitar ambiente')
+                        <li onclick="gesReserva()">
+                            <p><i class='fas fa-clipboard'></i> Gestionar mis solicitudes</p>
+                        
+                        </li>
+                    @endcan
                     <nav class="subMenu" id="sub3">
                         <ul>
                             <li>
@@ -87,14 +110,16 @@
                             </li>-->
                         </ul>
                     </nav>
-                    <li >
-                        <a href='{{ route('habilitarReservas') }}'><i class='fas fa-clipboard'></i> Gestionar Reservas</a>
-                        
-                    </li>
-                    <li>
-                        <a href='{{ route('publicaciones.index') }}'><i class='fas fa-clipboard'></i> Publicaciones</a>
-                        
-                    </li>
+                    @can('Confirmar reserva')
+                        <li >
+                            <a href='{{ route('habilitarReservas') }}'><i class='fas fa-clipboard'></i> Gestionar Reservas</a>
+                        </li>
+                    @endcan
+                    @can('Registrar publicacion')
+                        <li>
+                            <a href='{{ route('publicaciones.index') }}'><i class='fas fa-clipboard'></i> Publicaciones</a>
+                        </li>
+                    @endcan
                     <li onclick="GesRol()">
                         <p><i class='fas fa-clipboard'></i> Rol</p>
                     </li>
@@ -116,7 +141,8 @@
         </div>
         <div class="derecha">
             <header>
-                <h1 id="btnMenu" onclick="desMenu()"><i class='fas fa-bars'></i> Menu</h1>
+                <h1 id="btnMenu" onclick="desMenu()"><i class='fas fa-bars'></i> Menu </h1>
+                <h1><a href='{{ route('Usuario.show') }}'>Usuario:</a> {{ Auth::user()->nombre }}</h1>
                 <a href='{{ route('logout') }}'>  <i class='fas fa-user'></i> Salir </a>
             </header>
             <br>
