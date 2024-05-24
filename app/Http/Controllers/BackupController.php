00dd9cb923b905e0405e38fe3019cf0a65d8cc23
Katherine Ortiz <katherineortiz440@gmail.com>
2024-05-23 23:10:41 -0400

    backups-restore-funcional

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -27,70 +27,94 @@ class BackupController extends Controller
         } catch (\Exception $e) {
             return redirect()->back()->withErrors(['error' => 'No se pudo establecer la conexión a la base de datos']);
         }
-    
+
         // Obtener el nombre del archivo de backup
-        $backupName = date('d_m_Y_H-i-s') . '_backup.sql';
-    
+        $backupName = date('d-m-Y_H-i-s') . '_backup.sql';
+
         // Iniciar la construcción del archivo SQL
         $sql = "-- Respaldo de la base de datos $databaseName\n\n";
-    
-        // Agregar comando para deshabilitar las verificaciones de clave externa
-        $sql .= "SET FOREIGN_KEY_CHECKS=0;\n\n";
-    
-        // Agregar comando para crear la base de datos si no existe y seleccionarla
-        $sql .= "CREATE DATABASE IF NOT EXISTS $databaseName;\n";
-        $sql .= "USE $databaseName;\n\n";
-    
+
+        // Agregar configuraciones iniciales
+        $sql .= <<<EOL
+    /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+    /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+    /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+    /*!40101 SET NAMES utf8mb4 */;
+    /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+    /*!40103 SET TIME_ZONE='+00:00' */;
+    /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+    /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+    /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+    /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+    EOL;
+
         // Obtener el listado de tablas en la base de datos
         $tables = DB::select('SHOW TABLES');
-    
+
         foreach ($tables as $table) {
             $tableName = reset($table);
-    
-            if ($tableName == 'users') {
-                // No eliminar la tabla 'users', solo agregar los datos
-                $sql .= "-- Datos para la tabla $tableName\n\n";
-            } else {
-                // Agregar comando para eliminar la tabla si existe
-                $sql .= "DROP TABLE IF EXISTS $tableName;\n\n";
-    
-                $tableInfo = DB::select("SHOW CREATE TABLE $tableName");
-    
-                // Agregar comando para crear la tabla de nuevo
-                $sql .= $tableInfo[0]->{'Create Table'} . ";\n\n";
-            }
-    
+
+            // Agregar comando para eliminar la tabla si existe
+            $sql .= "DROP TABLE IF EXISTS `$tableName`;\n";
+
+            $tableInfo = DB::select("SHOW CREATE TABLE $tableName");
+
+            // Agregar comando para crear la tabla de nuevo
+            $sql .= "/*!40101 SET @saved_cs_client = @@character_set_client */;\n";
+            $sql .= "/*!40101 SET character_set_client = utf8 */;\n";
+            $sql .= $tableInfo[0]->{'Create Table'} . ";\n";
+            $sql .= "/*!40101 SET character_set_client = @saved_cs_client */;\n";
+
+            // Bloquear la tabla antes de insertar datos
+            $sql .= "LOCK TABLES `$tableName` WRITE;\n";
+            $sql .= "/*!40000 ALTER TABLE `$tableName` DISABLE KEYS */;\n";
+
             // Agregar los datos de la tabla al archivo SQL
             $tableData = DB::table($tableName)->get()->toArray();
             foreach ($tableData as $row) {
-                $sql .= "INSERT INTO $tableName VALUES (";
-    
+                $sql .= "INSERT INTO `$tableName` VALUES (";
+
                 // Formatear manualmente cada fila de datos
                 foreach ($row as $key => $value) {
                     // Si el valor es una cadena, escapar comillas
                     if (is_string($value)) {
                         $value = addslashes($value);
+                        $value = "'$value'";
+                    } elseif (is_null($value)) {
+                        $value = 'NULL';
                     }
-                    $sql .= "'$value', ";
+                    $sql .= "$value, ";
                 }
                 // Eliminar la última coma y espacio
                 $sql = rtrim($sql, ', ');
                 $sql .= ");\n";
             }
-            $sql .= "\n\n\n";
+
+            // Desbloquear la tabla después de insertar datos
+            $sql .= "/*!40000 ALTER TABLE `$tableName` ENABLE KEYS */;\n";
+            $sql .= "UNLOCK TABLES;\n\n";
         }
-    
-        // Agregar comando para habilitar las verificaciones de clave externa
-        $sql .= "SET FOREIGN_KEY_CHECKS=1;\n\n";
-    
+
+        // Agregar configuraciones finales
+        $sql .= <<<EOL
+    /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+    /*!40114 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+    /*!40101 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+    /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+    /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+    /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+    /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+    /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+    EOL;
+
         // Guardar el archivo de backup
         Storage::put('backup/' . $backupName, $sql);
-    
+
         return redirect()->back()->with('success', 'Copia de seguridad realizada con éxito');
     }
     
 
-public function restore(Request $request)
+    public function restore(Request $request)
     {
         $restorePoint = $request->input('restorePoint');
 
@@ -99,28 +123,20 @@ public function restore(Request $request)
 
         // Verifica si el archivo de backup existe
         if (Storage::exists($filePath)) {
-            $sqlStatements = explode(";\n", Storage::get($filePath));
+            $sqlStatements = explode(";", Storage::get($filePath));
             $totalErrors = 0;
             DB::statement('SET FOREIGN_KEY_CHECKS=0');
-
             foreach ($sqlStatements as $statement) {
-                if (!empty(trim($statement))) {
-                    // No ejecutar DROP TABLE para la tabla 'users'
-                    if (stripos($statement, 'DROP TABLE IF EXISTS users') !== false) {
-                        continue;
-                    }
-
-                    // Ejecución de otras declaraciones SQL
+                $statement = trim($statement);
+                if (!empty($statement)) {
                     try {
-                        DB::statement($statement);
+                        DB::statement($statement . ';');
                     } catch (\Exception $e) {
                         $totalErrors++;
                     }
                 }
             }
-
             DB::statement('SET FOREIGN_KEY_CHECKS=1');
-
             if ($totalErrors <= 0) {
                 return redirect()->back()->with('success', 'Restauración completada con éxito');
             } else {
